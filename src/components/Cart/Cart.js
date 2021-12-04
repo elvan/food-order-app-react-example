7dd303520dbf6f1520ac7f61b9ec8c8f635ccb8b
Elvan H <elvanhd@gmail.com>
2021-12-04 21:01:47 +0700

    working on the shopping cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,40 @@
+import classes from './Cart.module.css';
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[
+        {
+          id: 'c1',
+          name: 'Coffee',
+          price: 10,
+          amount: 2
+        },
+        {
+          id: 'c2',
+          name: 'Tea',
+          price: 5,
+          amount: 1
+        }
+      ].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>$35.75</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
