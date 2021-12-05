4bf63fe24391c4e085cbf983a4dceee4a28f20b6
Elvan H <elvanhd@gmail.com>
2021-12-05 17:03:29 +0700

    using the useEffect hook when adding or removing items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-  const cartContext = useContext(CartContext);
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
 
-  const numberOfCartItems = cartContext.items.reduce(
+  const { items } = useContext(CartContext);
+
+  const numberOfCartItems = items.reduce(
     (previous, item) => previous + item.amount,
     0
   );
 
+  const buttonClasses = `${classes.button} ${
+    buttonIsHighlighted ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonIsHighlighted(true);
+
+    const timeout = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onShowCart}>
+    <button className={buttonClasses} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
