f71dcabde03096ab8848e235fe6bffc2995646b5
Elvan H <elvanhd@gmail.com>
2022-01-12 11:42:30 +0700

    handling the loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,9 +5,12 @@ import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
+
       const response = await fetch(
         'https://react-http-ac3a7-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json'
       );
@@ -22,10 +25,19 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <div>Loading...</div>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
